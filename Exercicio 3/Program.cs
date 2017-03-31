441d52784520eddec93d7cda2c08146da080fc60
admeng.fitness <admeng.fitness@ENGAGRICOLA06SV>
2017-03-31 09:50:47 -0300

    Add project files.

---
+++ Exercicio 3/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Exercicio_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TextReader reader = new StreamReader("datas.txt");
+            //TextReader tr = new StreamReader(@"C:\Users\...\Desktop\Test.txt"); 
+            String linha = reader.ReadLine();
+            while (linha != null)
+            {
+
+                Console.WriteLine(linha.Substring(6));
+                linha = reader.ReadLine();
+
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
